045d5520319d870b314c01b3df2a5594bf1e9717
Jordy <Jordy@JORDY>
2016-10-10 11:50:25 -0600

    creacion de proyecto

---
+++ hrm_v4/hrm_v4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hrm_v4.MvcApplication" Language="C#" %>
---
+++ hrm_v4/hrm_v4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hrm_v4.Startup))]
+namespace hrm_v4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
